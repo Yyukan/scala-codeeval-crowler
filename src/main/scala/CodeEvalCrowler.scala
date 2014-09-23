e920e637ba02d61da9d1c2b00304d8858f6d51b6
oshtykhno <oshtykhno@asset-control.com>
2014-09-23 09:20:02 +0200

    - minor

--- src/main/scala/CodeEvalCrowler.scala
+++ src/main/scala/CodeEvalCrowler.scala
@@ -1,7 +1,23 @@
 
 
 object CodeEvalCrowler extends App {
-  println("CodeEvalCrowler started...")
+
+  val CODE_EVAL_URL = "www.codeeval.com"
+  val CODE_EVAL_LOGIN = "/accounts/login"
+
+  println("CodeEval Crowler started...")
+
+  if (args.length == 0) {
+    usage()
+    System.exit(-1)
+  }
+
+  println(s"Connecting $CODE_EVAL_URL$CODE_EVAL_LOGIN username ${args(0)}")
+
+
+  def usage(): Unit = {
+    println("CodeEvalCrowler <codeeval_login> <codeeval_password>")
+  }
 }
 
 
